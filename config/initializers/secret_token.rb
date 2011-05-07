9c485589aa29e40de0ff9ffae39455cf7ba21413
Michael Richardson <mcr@sandelman.ca>
2011-05-07 13:48:06 -0400

    bare rails3 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hourbank3::Application.config.secret_token = '9134e0acec30bda37e07fb847f6adbb87d7698516d244ce3a8393cf45d33e9780885567c42ff2e3cf89f29838bc56f1f7d14ff68035858039ef6a29a8ad15d57'
